1e773ec41d35b8949ae9deeeaded7ea23ef67bbd
sergio_valle <sergiovallegarma@gmail.com>
2024-06-26 20:34:03 -0500

    Agregar archivos de proyecto.

---
+++ Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class Conexion
+    {
+        public static string cadena1 = "Server=DESKTOP-KSJLU0B\\SQLNET;" +
+                        "Initial Catalog=WICMonitoriaDBDev; Integrated Security=True;trustservercertificate=True";
+
+        public static string cadena = "Server=DESKTOP-KSJLU0B\\SQLNET;" +
+                       "Initial Catalog=WICMonitoriaDBDev;trustservercertificate=True;" +
+            "User ID=UserMonitoria;Password=123456;";
+    }
+}
---
+++ Datos/DRegion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class DRegion
+    {
+        public void Registrar(string Nombre, bool Activo)
+        {
+
+            using (var connection = new SqlConnection(Conexion.cadena))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("USP_InsertRole", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                //Enviar los parámetros
+                SqlParameter parameter = new SqlParameter("@Name", SqlDbType.VarChar, 50);
+                parameter.Value = Nombre;
+                command.Parameters.Add(parameter);
+
+                SqlParameter parameter2 = new SqlParameter("@Enabled", SqlDbType.Bit);
+                parameter2.Value = Activo;
+                command.Parameters.Add(parameter2);
+
+                command.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
